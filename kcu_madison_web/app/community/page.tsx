3653b3d50cd386a183155aa1d313c001028e8848
Aimee Lee <lemyea0613@gmail.com>
2025-01-07 11:34:28 +0900

    implement community, projects, signup pages

---
+++ kcu_madison_web/app/community/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutKCU() {
+  return (
+    <div>
+      <h1>Community</h1>
+      <p>Community 페이지입니다.</p>
+    </div>
+  );
+}
---
+++ kcu_madison_web/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutKCU() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>그동안의 프로젝트들을 한눈에 볼 수 있는 프로젝트 페이지입니다.</p>
+    </div>
+  );
+}
